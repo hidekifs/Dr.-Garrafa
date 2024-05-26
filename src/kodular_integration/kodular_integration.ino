b65ef3ce48feb896117b8a8dfff1320519b5ca7b
hidekifs <fhideki_sato@hotmail.com>
2024-05-26 18:15:40 -0300

    Codigo correto

--- src/kodular_integration/kodular_integration.ino
+++ src/kodular_integration/kodular_integration.ino
@@ -23,34 +23,22 @@ void loop() {
     String dadosRecebidos = SerialBT.readStringUntil('\n'); // Lê os dados até encontrar um caractere de nova linha ('\n')
     Serial.println("Recebido: " + dadosRecebidos); // Imprime os dados recebidos no monitor serial
 
-    //extrai hora
+        //extrai hora
     int hora = extrairNumero(dadosRecebidos, "\"hora\":");
-    int minuto = extrairNumero(dadosRecebidos, "\"minuto\":");
     if (hora != -1) {
       Serial.print("Hora extraída: ");
-      Serial.print("Minuto extraído: ");
       Serial.println(hora);
-      Serial.println(minuto);
     } else {
       Serial.println("Nenhuma hora encontrada.");
     }
 
     //extrai minuto
-  //  int minuto = extrairNumero(dadosRecebidos, "\"minuto\":");
-  //  if (minuto != -1) {
-  //    Serial.print("Minuto extraído: ");
-  //    Serial.println(minuto);
-  //  } else {
-  //    Serial.println("Nenhum minuto encontrado.");
-  //  }
-
-    //extrai medicamento
-    int medicamento = extrairNumero(dadosRecebidos, "\"medicamento\":");
-    if (medicamento != -1) {
-      Serial.print("Medicamento extraído: ");
-      Serial.println(medicamento);
+    int minuto = extrairNumero(dadosRecebidos, "\"minuto\":");
+    if (minuto != -1) {
+      Serial.print("minuto extraída: ");
+      Serial.println(minuto);
     } else {
-      Serial.println("Nenhum medicamento encontrado.");
+      Serial.println("Nenhuma minuto encontrada.");
     }
 
     //extrai dispenser
@@ -61,6 +49,102 @@ void loop() {
     } else {
       Serial.println("Nenhum dispenser encontrado.");
     }
+
+    //extrai somente no dia
+    int uma_vez = extrairNumero(dadosRecebidos, "\"uma_vez\":");
+    if (uma_vez != -1) {
+      Serial.print("Uma vez extraído: ");
+      Serial.println(uma_vez);
+    }
+
+    //extrai repeticao
+    String repeticao = extrairString(dadosRecebidos, "\"repeticao\":");
+    if (repeticao != "") {
+      Serial.print("repeticao extraida: ");
+      Serial.println(repeticao);
+    }
+
+    //extrai dias de semana
+    String semana = extrairString(dadosRecebidos, "\"semana\":");
+    if (semana != "") {
+      Serial.print("Semana extraído: ");
+      Serial.println(semana);
+    }
+
+    //extrai segunda
+    int segunda = extrairNumero(dadosRecebidos, "\"segunda\":");
+    if (segunda != -1) {
+      Serial.print("Segunda extraído: ");
+      Serial.println(segunda);
+    }
+
+        //extrai terca
+    int terca = extrairNumero(dadosRecebidos, "\"terça\":");
+    if (terca != -1) {
+      Serial.print("Terça extraído: ");
+      Serial.println(terca);
+    }
+
+        //extrai quarta
+    int quarta = extrairNumero(dadosRecebidos, "\"quarta\":");
+    if (quarta != -1) {
+      Serial.print("quarta extraído: ");
+      Serial.println(quarta);
+    }
+
+            //extrai quinta
+    int quinta = extrairNumero(dadosRecebidos, "\"quinta\":");
+    if (quinta != -1) {
+      Serial.print("quinta extraído: ");
+      Serial.println(quinta);
+    }
+
+        //extrai sexta
+    int sexta = extrairNumero(dadosRecebidos, "\"sexta\":");
+    if (sexta != -1) {
+      Serial.print("sexta extraído: ");
+      Serial.println(sexta);
+    }
+
+            //extrai sabado
+    int sabado = extrairNumero(dadosRecebidos, "\"sabado\":");
+    if (sabado != -1) {
+      Serial.print("sabado extraído: ");
+      Serial.println(sabado);
+    }
+
+            //extrai domingo
+    int domingo = extrairNumero(dadosRecebidos, "\"domingo\":");
+    if (domingo != -1) {
+      Serial.print("domingo extraído: ");
+      Serial.println(domingo);
+    }
+  }
+}
+
+// Declaração da função extrairString
+String extrairString(String dados, String parametro) {
+  int startIndex = dados.indexOf(parametro);
+  if (startIndex == -1) {
+    return ""; // Retorna uma string vazia se o parâmetro não for encontrado
+  }
+  
+  startIndex += parametro.length(); // Move para o próximo caractere após o parâmetro
+  
+  // Pular qualquer espaço em branco
+  while (startIndex < dados.length() && dados[startIndex] != '\"') {
+    startIndex++;
+  }
+  
+  startIndex++; // Avança para o próximo caractere após a primeira aspas
+  
+  // Encontra o índice do próximo caractere de aspas
+  int endIndex = dados.indexOf('\"', startIndex);
+  
+  if (endIndex != -1) {
+    return dados.substring(startIndex, endIndex); // Retorna a substring entre as aspas
+  } else {
+    return ""; // Retorna uma string vazia se o segundo conjunto de aspas não for encontrado
   }
 }
 
